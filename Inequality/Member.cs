671156dbeaddc061a19b851eecc423df82ad5826
artem <platonovartem2005@outlook.com>
2020-04-12 17:31:05 +0300

    Add class Bracket, Member, Operations.

---
+++ Inequality/Member.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inequality
+{
+    class Member : Variable
+    {
+        public int priority = 0;
+        public Member(string str)
+        {
+            
+        }
+    }
+}
---
+++ Inequality/Operations.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inequality
+{
+    static class Operations
+    {
+        public static Member Multiply(Member m1, Member m2)
+        {
+            /*
+             Multiply code
+            */
+            return m1;
+        }
+        public static Member Multiply(Member m1, List<Member> m2)
+        {
+            /*
+             Multiply code
+            */
+            return m1;
+        }
+        public static Member Multiply(List<Member> m1, Member m2)
+        {
+            /*
+             Multiply code
+            */
+            return m1[0];
+        }
+        public static Member Multiply(List<Member> m1, List<Member> m2)
+        {
+            /*
+             Multiply code
+            */
+            return m1[0];
+        }
+
+
+        public static Member Division(Member m1, Member m2)
+        {
+            /*
+             Division code
+            */
+            return m1;
+        }
+        public static Member Division(Member m1, List<Member> m2)
+        {
+            /*
+             Division code
+            */
+            return m1;
+        }
+        public static Member Division(List<Member> m1, Member m2)
+        {
+            /*
+             Division code
+            */
+            return m2;
+        }
+        public static Member Division(List<Member> m1, List<Member> m2)
+        {
+            /*
+             Division code
+            */
+            return m1[0];
+        }
+    }
+}
